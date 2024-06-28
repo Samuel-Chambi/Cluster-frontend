6186c6eb5c7ea2ab592ec2555044954c105f56be
AlejandrVilla <avillah@unsa.edu.pe>
2024-06-28 11:25:05 -0500

    shell

--- install.sh
+++ install.sh
@@ -1,22 +1,13 @@
 #! /bin/bash
 
-mkdir cluster
-cd cluster
-
 sudo yum update -y
 sudo yum install git -y
-echo "git version"
-git --version
 
 python --version
 
 pip --version
 
-pip install Flask
-pip install virtualenv
-pip install pyspark
-
-git clone git@github.com:Samuel-Chambi/Cluster-frontend.git
-cd Cluster-frontend
-
 virtualenv -p python3.9 env
+source env/bin/activate
+pip install Flask
+pip install pyspark
